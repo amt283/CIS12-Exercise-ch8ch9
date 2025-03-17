89300ef1f3978841cae0ec4d9cb50dcc1913b600
amt954 <athompson283@gmail.com>
2025-03-16 22:56:21 -0700

    Completed 9.15.4

---
+++ 9-15-4.py
@@ -0,0 +1,17 @@
+"""Write a function called reverse_sentence that takes as an argument a string that contains any number of words
+separated by spaces. It should return a new string that contains the same words in reverse order. For example, if the
+argument is “Reverse this sentence”, the result should be “Sentence this reverse”.
+
+Hint: You can use the capitalize methods to capitalize the first word and convert the other words to lowercase."""
+
+def main():
+    print(reverse_sentence('This is what they say in reverse'))
+
+# Reverses sentence word order and returns the reversed sentence with capitalized first word
+def reverse_sentence(phrase):
+    split_phrase = phrase.split()
+    reversed_split = reversed(split_phrase)
+    return ' '.join(reversed_split).capitalize()
+
+if __name__ == "__main__":
+    main()
